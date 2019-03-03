07b62d381580f152f7f930abf3c3247177687e68
thienhoang563 <thienhoang563@gmail.com>
2019-03-03 18:53:06 +0700

    i18N

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thienhmp
+ * Date: 03/03/2019
+ * Time: 18:45
+ */
+
+return [
+    'title' => 'Message was written by English',
+];
---
+++ resources/lang/vi/messages.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thienhmp
+ * Date: 03/03/2019
+ * Time: 18:46
+ */
+return [
+    'title' => 'Tin nhan Vietnamese',
+];
